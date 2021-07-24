93779b173d7eceb83e64f93a57937ef0e52ab347
ValentinaVaron <jvaronva@ibero.edu.co>
2021-07-23 21:18:59 -0500

    Proyecto Final Unidad1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+function mensajealerta() {
+    alert("DEJÁNOS SABER TU EXPERIENCIA EN NUESTRA PÁGINA DILIGENCIANDO EL SIGUIENTE FORMULARIO")
+
+}
+    
+function economiacolombia() {
+    alert("ECONOMÍA MIXTA EN COLOMBIA. SECTOR PÚBLICO Y PRIVADO")
+
+}
+
+function adamsmith() {
+    alert("ADAM SMITH 1723 - 1790")
+
+}
+
+function johnkeynes() {
+    alert("JOHN KEYNES 1883 - 1946")
+
+}
+
+function miltonfriedman() {
+    alert("MILTON FRIEDMAN 1912 - 2006")
+
+}
